058a0ea7d49da3cb060ebdc99cdecd689f77acef
Ravil Galeyev <dehasi@yandex.ru>
2021-07-09 00:05:55 +0200

    CH-15

--- lib/ch_15-hollywood.rb
+++ lib/ch_15-hollywood.rb
@@ -30,3 +30,31 @@ class WordFrequencyFramework
     end
   end
 end
+
+# The entities of the application
+class DataStorage
+
+  def initialize(wfapp, stop_words_filter)
+    @stop_words_filter = stop_words_filter
+    wfapp.register_for_load_event(-> { load })
+    wfapp.register_for_dowork_event(-> { produce_words })
+  end
+
+  def load()
+    @data = File.read(path_to_file).gsub(/[\W_]+/, ' ').downcase.split
+  end
+
+  def produce_words()
+    for w in @data.split
+      if not @stop_words_filter.stop_word? w
+        for h in @word_even_handlers
+          h.call(w)
+        end
+      end
+    end
+  end
+
+  def regirster_for_word_event(handler)
+    @word_even_handlers.append(handler)
+  end
+end
