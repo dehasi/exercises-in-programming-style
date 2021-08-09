7fd90a6300a76537b3258a7e10ae900186c2a34f
Ravil Galeyev <dehasi@yandex.ru>
2021-08-09 20:35:25 +0200

    CH-32

--- test/pride-and-prejudice_test.rb
+++ test/pride-and-prejudice_test.rb
@@ -42,7 +42,7 @@ class PrideAndPrejudiceTest < Test::Unit::TestCase
   end
 
   def test_single_style
-    run_single_script("#{WORK_DIR}/lib/ch_31-map-reduce.rb")
+    run_single_script("#{WORK_DIR}/lib/ch_32-double-map-reduce.rb")
   end
 
   def teardown
