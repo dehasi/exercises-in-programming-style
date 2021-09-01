14a73abe6eb4096c28fc186bafa5ca7edac225ff
Ravil Galeyev <dehasi@yandex.ru>
2021-09-01 21:03:11 +0200

    CH-35

--- nn/lib/ch_35-dense-shallow-under-control.rb
+++ nn/lib/ch_35-dense-shallow-under-control.rb
@@ -1,11 +1,9 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-CHARACTERS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c'
-# char_indices = dict((c, i) for i, c in enumerate(characters))
-# indices_char = dict((i, c) for i, c in enumerate(characters))
+CHARACTERS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c"
+char_indices = CHARACTERS.chars.each_with_index.map { |ch, i| [ch, i] }.to_h
+indices_char = CHARACTERS.chars.each_with_index.map { |ch, i| [i, ch] }.to_h
 
 INPUT_VOCAB_SIZE = CHARACTERS.size
 
-ss = CHARACTERS.each_with_index { |item, index| [item, index] }.to_h
-puts ss.inspect
