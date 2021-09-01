13377be12ea71c6a3789604385a909606918083e
Ravil Galeyev <dehasi@yandex.ru>
2021-09-01 21:06:16 +0200

    CH-35

--- nn/lib/ch_35-dense-shallow-under-control.rb
+++ nn/lib/ch_35-dense-shallow-under-control.rb
@@ -2,8 +2,20 @@
 # frozen_string_literal: true
 
 CHARACTERS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c"
-char_indices = CHARACTERS.chars.each_with_index.map { |ch, i| [ch, i] }.to_h
-indices_char = CHARACTERS.chars.each_with_index.map { |ch, i| [i, ch] }.to_h
+CHAR_INDICES = CHARACTERS.chars.each_with_index.map { |ch, i| [ch, i] }.to_h
+INDICES_CHAR = CHARACTERS.chars.each_with_index.map { |ch, i| [i, ch] }.to_h
 
 INPUT_VOCAB_SIZE = CHARACTERS.size
 
+def encode_one_hot(line)
+  x = np.zeros(line.size, INPUT_VOCAB_SIZE)
+  line.chars.each_with_index do |c, i|
+    if CHARACTERS.include? c
+      index = CHAR_INDICES[c]
+    else
+      index = CHAR_INDICES[' ']
+    end
+    x[i][index] = 1
+  end
+  x
+end
