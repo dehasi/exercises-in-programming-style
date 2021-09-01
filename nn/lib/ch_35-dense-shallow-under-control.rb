9c61508ffd748da21890fa956b4ec8a18a86932f
Ravil Galeyev <dehasi@yandex.ru>
2021-09-01 21:17:27 +0200

    CH-35

--- nn/lib/ch_35-dense-shallow-under-control.rb
+++ nn/lib/ch_35-dense-shallow-under-control.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'matrix'
+
 CHARACTERS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c"
 CHAR_INDICES = CHARACTERS.chars.each_with_index.map { |ch, i| [ch, i] }.to_h
 INDICES_CHAR = CHARACTERS.chars.each_with_index.map { |ch, i| [i, ch] }.to_h
@@ -8,14 +10,17 @@ INDICES_CHAR = CHARACTERS.chars.each_with_index.map { |ch, i| [i, ch] }.to_h
 INPUT_VOCAB_SIZE = CHARACTERS.size
 
 def encode_one_hot(line)
-  x = np.zeros(line.size, INPUT_VOCAB_SIZE)
+  x = Matrix.build(line.size, INPUT_VOCAB_SIZE) { 0 }
   line.chars.each_with_index do |c, i|
     if CHARACTERS.include? c
       index = CHAR_INDICES[c]
     else
       index = CHAR_INDICES[' ']
     end
-    x[i][index] = 1
+    x[i, index] = 1
   end
   x
 end
+
+r = encode_one_hot("123")
+puts r.inspect
