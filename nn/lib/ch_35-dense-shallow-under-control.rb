92b35b3cab5625a02c861f76a8123cfa4dfae5ac
Ravil Galeyev <dehasi@yandex.ru>
2021-09-01 21:39:22 +0200

    CH-35

--- nn/lib/ch_35-dense-shallow-under-control.rb
+++ nn/lib/ch_35-dense-shallow-under-control.rb
@@ -3,6 +3,9 @@
 
 require 'matrix'
 
+ASCII_LOWERCASE = 'abcdefghijklmnopqrstuvwxyz'.chars # Python's list(string.ascii_lowercase)
+ASCII_UPPERCASE = ASCII_LOWERCASE.upcase
+
 CHARACTERS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c"
 CHAR_INDICES = CHARACTERS.chars.each_with_index.map { |ch, i| [ch, i] }.to_h
 INDICES_CHAR = CHARACTERS.chars.each_with_index.map { |ch, i| [i, ch] }.to_h
@@ -26,6 +29,18 @@ def decode_one_hot(x)
   x.map { |onehot| onehot.rindex(onehot.max) }
    .map { |one_index| INDICES_CHAR[one_index] }
    .join ''
+end
+
+def normalization_layer_set_weights(n_layer)
+  wb = []
+  w = Array.new(INPUT_VOCAB_SIZE, 0.0) { Array.new(INPUT_VOCAB_SIZE, 0.0) }
+  b = Array.new(INPUT_VOCAB_SIZE, 0.0)
+  # Let lower case letters go through
+  # Let lower case letters go through
+  ASCII_LOWERCASE.each do |c|
+    i = CHAR_INDICES[c]
+    w[i][i] = 1
+  end
 end
 
 r = encode_one_hot("123")
